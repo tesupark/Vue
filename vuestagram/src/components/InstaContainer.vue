32ef9c7019decf44d932dcbf96d727ff51e73eb1
tesupark <tesupark00@gmail.com>
2024-07-26 16:55:29 +0900

    vueinsta 이중 props

---
+++ vuestagram/src/components/InstaContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Post :instaData="a" v-for="(a, i) in instaData" :key="i"/>
+  </div>
+</template>
+
+<script>
+import PostVue from './InstaPost.vue';
+
+export default {
+  props: {
+    instaData: Array,
+  },
+  components: {
+    Post: PostVue,
+  }
+}
+</script>
+
+<style>
+
+</style>
